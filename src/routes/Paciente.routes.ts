3b43fc3333c2dbd68fd701e0d92aaa868a9a710d
Paulo César Coronado <paulocoronado@udistrital.edu.co>
2023-04-26 02:40:29 -0500

    v 0.0.0.2

--- src/routes/Paciente.routes.ts
+++ src/routes/Paciente.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import PacienteController from '../controllers/pacienteController';
 
 class PacienteRouter {
@@ -12,7 +12,9 @@ class PacienteRouter {
   }
 
   private routes(): void {
-    this.router.get('/pacientes', this.pacienteController.getAllUsers.bind(this.pacienteController));
+    this.router.get('/pacientes', (req: Request, res: Response) => {
+      this.pacienteController.getAllPatients(req, res);
+    });
     this.router.post('/crear_paciente', this.pacienteController.createUser.bind(this.pacienteController));
   }
 }
